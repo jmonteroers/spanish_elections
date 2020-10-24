314a862ad48965bbde392aa67d0854bcfd48a655
Juan Montero <jmonteroers@gmail.com>
2020-10-25 01:19:27 +0200

    simplify __init__, add load_data module

--- spanish_elections/__init__.py
+++ spanish_elections/__init__.py
@@ -1,3 +1,4 @@
-from .prepare_results import extract_general_data, extract_results_by_province
 from .dhondt_rule import dhondt_rule_long
 from .transfer_votes import transfer_votes_long
+from . import load_data
+from . import summarise
