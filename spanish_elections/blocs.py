b4a1065a40e126e217621e5fb4738e95b2c83c24
Juan <juan@pxhealthcare.com>
2021-09-29 20:39:57 +0100

    Fix errors in blocs code and 2020 investment bloc data

--- spanish_elections/blocs.py
+++ spanish_elections/blocs.py
@@ -1,8 +1,8 @@
 from collections import defaultdict
-from typing import TextIO
+from typing import TextIO, Dict
 from pathlib import Path
 
-from spanish_elections.data import read_as_json
+from spanish_elections.load import read_as_json
 
 
 blocs_path = Path(__file__).parent / "data" / "blocs"
@@ -11,13 +11,15 @@ class Bloc:
     def __init__(self, blocs: Dict[str, str]):
         self.from_bloc_to_pparty = blocs
         self.from_pparty_to_bloc = {
-            pparty: bloc for bloc, pparty in blocs.items()
+            pparty: bloc
+            for bloc, pparties in blocs.items()
+            for pparty in pparties
         }
 
-    def get_results_by_bloc(self, res: Dict[str, int]):
-        results_by_bloc = defaultdict(0)
+    def get_results_by_bloc(self, res: Dict[str, int]) -> Dict[str, int]:
+        results_by_bloc = defaultdict(int)
         for pparty, value in res.items():
-            results_by_bloc[pparty] += value
+            results_by_bloc[self.from_pparty_to_bloc.get(pparty)] += value
         return results_by_bloc
 
 
