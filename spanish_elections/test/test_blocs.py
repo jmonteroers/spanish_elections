ac54662c0ccda24fe1d3c58099a196d50edf38c7
Juan <juan@pxhealthcare.com>
2021-10-05 18:49:08 +0100

    Fix errors in test blocs

--- spanish_elections/test/test_blocs.py
+++ spanish_elections/test/test_blocs.py
@@ -5,11 +5,10 @@ def test_investment_2020():
     seats_nov_2019 = load_election_results("2019").get_seats_by_pparty()
     bloc_inv_2020 = load_bloc_by_name("investment_2020")
     bloc_seats_nov_2019 = bloc_inv_2020.get_results_by_bloc(seats_nov_2019)
-    breakpoint()
     assert bloc_seats_nov_2019["pro"] > bloc_seats_nov_2019["against"], \
         "2020 Investment must have more votes in favour than against"
     assert bloc_seats_nov_2019 == {
-        "pro": 165,
+        "pro": 166,
         "against": 164,
         "abstain": 18,
         None: 2
